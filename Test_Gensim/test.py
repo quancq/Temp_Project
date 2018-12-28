6c74e33681c93f277098956b43b2f5ad998c5fc6
quancq <quancq.it@gmail.com>
2018-12-28 18:40:01 +0700

    Update

--- Test_Gensim/test.py
+++ Test_Gensim/test.py
@@ -1,4 +1,7 @@
 from gensim.models import Word2Vec, FastText
+import utils
+import time
+import pandas as pd
 
 
 def test():
@@ -17,38 +20,54 @@ def test():
     #     "Máy tính Lenovo Thinkpad"
     # ]
 
-    product_name = "Samsung"
+    # product_name = "Samsung"
+    #
+    # candidate_names = [
+    #     "Apple",
+    #     "Samsung",
+    #     "Dell",
+    #     "Casio",
+    #     "Honda",
+    #     "Yamaha",
+    #     "Gucci",
+    #     "Lenovo",
+    #     "Electrolux",
+    #     "Xiaomi",
+    #     "Daikin",
+    #     "Kangaroo",
+    #     "Sony",
+    #     "Downy",
+    #     "TH True Milk",
+    #     "Panasonic",
+    #     "LG",
+    #     "Hitachi",
+    #     "Toshiba",
+    # ]
 
-    candidate_names = [
-        "Apple",
-        "Samsung",
-        "Dell",
-        "Casio",
-        "Honda",
-        "Yamaha",
-        "Gucci",
-        "Lenovo",
-        "Electrolux",
-        "Xiaomi",
-        "Daikin",
-        "Kangaroo",
-        "Sony",
-        "Downy",
-        "TH True Milk",
-        "Panasonic",
-        "LG",
-        "Hitachi",
-        "Toshiba",
-    ]
+    product_name = "Điện thoại Samsung Galaxy A7 (2018) SM-A750G Black"
+
+    test_dir = "./Dataset/Preprocess/Train"
+    test_paths = utils.get_file_paths(test_dir)
+    test_df = utils.load_csvs(test_paths)
+    candidate_names = test_df["Model"].values.tolist()
+
     result = []
-
-    for candidate_name in candidate_names:
+    start_time = time.time()
+    for i, candidate_name in enumerate(candidate_names):
         sim = model.similarity(product_name, candidate_name)
         result.append((product_name, candidate_name, sim))
 
+        if (i+1) % 10 == 0:
+            print("Calculate similarity {}/{} done".format(i+1, len(candidate_names)))
+
     result.sort(key=lambda x: x[2], reverse=True)
-    for name1, name2, sim in result:
-        print("({} - {}) : {}".format(name1, name2, sim))
+    # for name1, name2, sim in result:
+        # print("\n{}\n{}\n{}\n".format(name1, name2, sim))
+    result_df = pd.DataFrame(result[:1000], columns=["Product_Name", "Candidate_Name", "Similarity"])
+    save_path = "./Result/test_similarity.csv"
+    utils.save_csv(result_df, save_path)
+    exec_time = time.time() - start_time
+    print("Time : {:.2f} seconds".format(exec_time))
 
 
 if __name__ == "__main__":
