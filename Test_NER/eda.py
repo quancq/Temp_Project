ceecdff7ba3d48ed88d4bf30c5647e9d5e61e782
quancq <quancq.it@gmail.com>
2019-01-09 12:00:28 +0700

    Update

--- Test_NER/eda.py
+++ Test_NER/eda.py
@@ -5,39 +5,44 @@ import time
 
 
 class DatasetManager:
-    def __init__(self, dataset_path):
+    def __init__(self, dataset_path="./Dataset/data.json"):
         self.dataset_path = dataset_path
-        self.load_data()
+        if dataset_path.endswith(".csv"):
+            self.load_csv_data()
+        elif dataset_path.endswith(".json"):
+            self.load_json_data()
+        else:
+            print("Dataset path {} is not valid".format(dataset_path))
 
-    def load_data(self):
+    def load_csv_data(self):
         start_time = time.time()
         self.df = utils.load_csv(self.dataset_path, encoding="latin1")
         self.df.fillna(method="ffill", inplace=True)
         print(self.df.info())
-        # sentences = {}
-        # for i, row in self.df.iterrows():
-        #     curr_sentence_id = int(row["Sentence #"].split(" ")[1])
-        #     sentence = sentences.get(curr_sentence_id)
-        #     if sentence is None:
-        #         sentence = ([], [], [])
-        #         sentences.update({curr_sentence_id: sentence})
-        #
-        #     word, pos, tag = row["Word"], row["POS"], row["Tag"]
-        #     sentence[0].append(word)
-        #     sentence[1].append(pos)
-        #     sentence[2].append(tag)
-        #
-        # sentence_len = [len(sent[0]) for sent in list(sentences.values())]
-        # min_len, max_len, avg_len = np.min(sentence_len), np.max(sentence_len), np.average(sentence_len)
-        # print("Min sentence length : ", min_len)
-        # print("Max sentence length : ", max_len)
-        # print("Avg sentence length : ", avg_len)
-        #
-        # self.sentences = sentences
+        sentences = {}
+        for i, row in self.df.iterrows():
+            curr_sentence_id = int(row["Sentence #"].split(" ")[1])
+            sentence = sentences.get(curr_sentence_id)
+            if sentence is None:
+                sentence = ([], [], [])
+                sentences.update({curr_sentence_id: sentence})
+
+            word, pos, tag = row["Word"], row["POS"], row["Tag"]
+            sentence[0].append(word)
+            sentence[1].append(pos)
+            sentence[2].append(tag)
+
+        sentence_len = [len(sent[0]) for sent in list(sentences.values())]
+        min_len, max_len, avg_len = np.min(sentence_len), np.max(sentence_len), np.average(sentence_len)
+        print("Min sentence length : ", min_len)
+        print("Max sentence length : ", max_len)
+        print("Avg sentence length : ", avg_len)
+
+        self.sentences = sentences
         self.words = self.df["Word"].values.tolist()
         self.poses = self.df["POS"].values.tolist()
         self.tags = self.df["Tag"].values.tolist()
-        # print("Number sentences    : ", len(sentences))
+        print("Number sentences    : ", len(sentences))
         self.unique_words = list(set(self.words))
         print("Number unique WORDs : ", len(self.unique_words))
         self.unique_poses = list(set(self.poses))
@@ -49,6 +54,14 @@ class DatasetManager:
 
         # save_json_path = "./Dataset/data.json"
         # utils.save_json(self.sentences, save_json_path)
+
+        exec_time = time.time() - start_time
+        print("Load data done. Time {:.2f} seconds".format(exec_time))
+
+    def load_json_data(self):
+        start_time = time.time()
+        self.sentences = utils.load_json(self.dataset_path)
+        print("Number sentences    : ", len(self.sentences))
 
         exec_time = time.time() - start_time
         print("Load data done. Time {:.2f} seconds".format(exec_time))
---
+++ Test_NER/models/crf.py
@@ -0,0 +1,124 @@
+import utils
+from eda import DatasetManager
+import numpy as np
+from sklearn_crfsuite import CRF
+from sklearn_crfsuite.metrics import flat_f1_score
+from sklearn_crfsuite import scorers, metrics
+from sklearn.model_selection import RandomizedSearchCV, cross_validate, cross_val_score, cross_val_predict
+from sklearn.metrics import make_scorer, classification_report
+import time
+
+
+def word2features(sent, i):
+    word = sent[i][0]
+    postag = sent[i][1]
+
+    features = {
+        'bias': 1.0,
+        'word.lower()': word.lower(),
+        'word[-3:]': word[-3:],
+        'word[-2:]': word[-2:],
+        'word.isupper()': word.isupper(),
+        'word.istitle()': word.istitle(),
+        'word.isdigit()': word.isdigit(),
+        'postag': postag,
+        'postag[:2]': postag[:2],
+    }
+    if i > 0:
+        word1 = sent[i-1][0]
+        postag1 = sent[i-1][1]
+        features.update({
+            '-1:word.lower()': word1.lower(),
+            '-1:word.istitle()': word1.istitle(),
+            '-1:word.isupper()': word1.isupper(),
+            '-1:postag': postag1,
+            '-1:postag[:2]': postag1[:2],
+        })
+    else:
+        features['BOS'] = True
+
+    if i < len(sent)-1:
+        word1 = sent[i+1][0]
+        postag1 = sent[i+1][1]
+        features.update({
+            '+1:word.lower()': word1.lower(),
+            '+1:word.istitle()': word1.istitle(),
+            '+1:word.isupper()': word1.isupper(),
+            '+1:postag': postag1,
+            '+1:postag[:2]': postag1[:2],
+        })
+    else:
+        features['EOS'] = True
+
+    return features
+
+
+def sent2features(sent):
+    return [word2features(sent, i) for i in range(len(sent))]
+
+
+def sent2labels(sent):
+    return [label for token, postag, label in sent]
+
+
+def sent2tokens(sent):
+    return [token for token, postag, label in sent]
+
+
+def main():
+    dm = DatasetManager(dataset_path="../Dataset/data.json")
+    sentences = list(dm.sentences.values())
+    sent_len = len(sentences)
+    train_size = int(sent_len * 0.8)
+    print('Num Train size : ', train_size)
+    print('Num Test size : ', (sent_len - train_size))
+
+    train_sents = sentences[:train_size]
+    test_sents = sentences[train_size:]
+    X_train = [sent2features(sent) for sent in train_sents]
+    y_train = [sent2labels(sent) for sent in train_sents]
+    X_test = [sent2features(sent) for sent in test_sents]
+    y_test = [sent2labels(sent) for sent in test_sents]
+
+    print("X_train size : ", len(X_train))
+    print("X_test size : ", len(X_test))
+
+    start_time = time.time()
+    crf = CRF(algorithm='lbfgs',
+              c1=0.1,
+              c2=0.1,
+              max_iterations=100,
+              all_possible_transitions=True)
+    crf.fit(X_train, y_train)
+    labels = list(crf.classes_)
+    labels.remove('O')
+
+    y_pred = crf.predict(X_test)
+    macro_score = flat_f1_score(y_test, y_pred, average='macro', labels=labels)
+    micro_score = flat_f1_score(y_test, y_pred, average='micro', labels=labels)
+    weight_score = flat_f1_score(y_test, y_pred, average='weighted', labels=labels)
+    print("F1_macro_score : ", macro_score)
+    print("F1_micro_score : ", micro_score)
+    print("F1_weight_score : ", weight_score)
+
+    # scoring = ["f1_macro", "precision_macro", "recall_macro"]
+    # scores = cross_validate(crf, X=X_train, y=y_train, scoring=scoring, cv=2, n_jobs=-1)
+    # print("f1_macro        : ", np.mean(scores["test_f1_macro"]))
+    # print("precision_marco : ", np.mean(scores["test_precision_macro"]))
+    # print("recall_macro    : ", np.mean(scores["test_recall_macro"]))
+
+    # group B and I results
+    sorted_labels = sorted(
+        labels,
+        key=lambda name: (name[1:], name[0])
+    )
+    print(metrics.flat_classification_report(
+        y_test, y_pred, labels=sorted_labels, digits=3
+    ))
+
+    exec_time = time.time() - start_time
+    print("\nExec Time : {:.2f} seconds".format(exec_time))
+
+
+if __name__ == "__main__":
+    main()
