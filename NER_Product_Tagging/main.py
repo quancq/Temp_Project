4f7596a2651a837e626dff93d753c870dd46aa82
quancq <quancq.it@gmail.com>
2019-02-11 12:05:54 +0700

    Update

--- NER_Product_Tagging/main.py
+++ NER_Product_Tagging/main.py
@@ -1,6 +1,8 @@
 import utils
 import pandas as pd
 import os, json, time
+import re
+from pyvi import ViPosTagger
 
 
 def old_main():
@@ -119,5 +121,79 @@ def main():
     print("Time : {:.2f} seconds".format(exec_time))
 
 
+def build_ner_dataset():
+    fpath = "./Data/output/Doc_by_lines/doc_1"
+    raw_lines = utils.load_str(fpath).split("\n")
+    num_docs = 0
+    max_docs = 20
+    num_sents = 0
+
+    tokens_of_doc = []
+    ner_tags_of_doc = []
+    data = []
+    p = re.compile("[?!.]+")
+    for line in raw_lines:
+        if line.startswith("<Product"):
+            tokens_of_doc = []
+            ner_tags_of_doc = []
+
+        elif line.startswith("</Product>"):
+            num_docs += 1
+            doc = " ".join(tokens_of_doc)
+            post_tags_of_doc = ViPosTagger.postagging(doc)[1]
+
+            # Assign sent id
+            sent_ids = []
+            is_end_sent = True
+            for token in tokens_of_doc:
+                if p.search(token):
+                    is_end_sent = True
+                else:
+                    if is_end_sent:
+                        num_sents += 1
+                    is_end_sent = False
+                sent_ids.append(num_sents)
+
+            for token_id in range(len(tokens_of_doc)):
+                sent_id, token, ner_tag, post_tag = sent_ids[token_id], tokens_of_doc[token_id], \
+                                                    ner_tags_of_doc[token_id], post_tags_of_doc[token_id]
+                if ner_tag != "O":
+                    if token_id > 0 and ner_tags_of_doc[token_id-1] == ner_tag \
+                            and sent_ids[token_id-1] == sent_ids[token_id]:
+                        prefix = "I-"
+                    else:
+                        prefix = "B-"
+                    map_convert_tag = {"Category": "Cat",
+                                       "Model": "Mod",
+                                       "Info": "Inf",
+                                       "Brand": "Bra",
+                                       "O": "O"}
+                    ner_tag = map_convert_tag[ner_tag]
+                    ner_tag = prefix + ner_tag
+                data.append((num_docs, sent_id, token, post_tag, ner_tag))
+
+            if num_docs >= max_docs:
+                break
+
+        else:
+            idx = line.rfind(",")
+            token = line[:idx]
+            ner_tag = line[idx+1:]
+            if token == "":
+                continue
+            tokens_of_doc.append(token)
+            ner_tag = ner_tag if len(ner_tag) > 0 else "O"
+            ner_tags_of_doc.append(ner_tag)
+
+    data = pd.DataFrame(data, columns=["Doc_Id", "Sent_Id", "Token", "Post_Tag", "Ner_Tag"])
+    print(data.head(10))
+    print("\n==================")
+    print(data.tail(10))
+    print(data.shape[0])
+
+    save_path = "./Data/Ner_Dataset/ner.csv"
+    utils.save_csv(data, save_path)
+
+
 if __name__ == "__main__":
-    main()
+    build_ner_dataset()
